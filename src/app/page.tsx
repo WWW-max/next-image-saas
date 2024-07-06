23cab0981170d7c38f577d89654944e75e63efae
monster-wu <464711201@qq.com>
2024-07-06 13:32:46 +0800

    feat: first page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,19 @@
+import { Button } from "@/components/Button";
+import { Input } from "@/components/Input";
+import { Textarea } from "@/components/Textarea";
 import Image from "next/image";
 
 export default function Home() {
   return (
-    <main>
-      root
-    </main>
+    <div className="h-screen flex justify-center items-center">
+      <form className="w-full max-w-md flex flex-col gap-4">
+        <Input name="name" placeholder="Add APP"></Input>
+        <Textarea
+          name="description"
+          placeholder="Description"
+        ></Textarea>
+        <Button type="submit">Submit</Button>
+      </form>
+    </div>
   );
 }
